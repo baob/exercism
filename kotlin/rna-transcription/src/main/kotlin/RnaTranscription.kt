b633c621aef199531ae3f51094f2fa92c99ac3d2
baob <coder@onesandthrees.com>
2018-02-07 17:23:14 +0000

    rna transcription iteration 1 - 2 January 2018 at 17:13 GMT

--- kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,11 +1,10 @@
-
-fun transcribeToRna(dna: String): String                                                                                                                                                                                                                                                                        {
-    return dna.toCharArray().joinToString("") { dnaToRnaMap[it] ?: "(no mapping for $it)" }
-}
-
 private val dnaToRnaMap = hashMapOf(
-        'C' to "G",
-        'G' to "C",
-        'T' to "A",
-        'A' to "U"
+        "C" to "G",
+        "G" to "C",
+        "T" to "A",
+        "A" to "U"
 )
+
+fun transcribeToRna(dna: String): String = dna.map{ transcribeSingleDnaToRna(it.toString())}.joinToString("")
+
+private fun transcribeSingleDnaToRna(dna: String): String = dnaToRnaMap[dna] ?: "(no mapping for $dna)"
