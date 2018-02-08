97a13307e73d3ebedcf2f38ccda640832ebc4d09
baob <coder@onesandthrees.com>
2018-02-08 16:59:33 +0000

    secret-handshake - exercism 4 - map direct from binary

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,28 +1,31 @@
-import kotlin.experimental.and
-
 class HandshakeCalculator {
 
     companion object {
 
-        val mapBinaryPlaceToSignal = Signal.values().map{ Pair(it.ordinal, it) }.toMap()
+        private val mapBinaryPlaceToSignal: Map<Int, Signal?> = mapOf(
+                1 to Signal.WINK,
+                2 to Signal.DOUBLE_BLINK,
+                4 to Signal.CLOSE_YOUR_EYES,
+                8 to Signal.JUMP,
+                16 to null
+        )
 
         fun calculateHandshake(i: Int): List<Signal> {
             val binaryPlaces = intToBinaryPlaceValues(i)
-            return mapBinaryPlacesToSignals(binaryPlaces)
+            val baseList = baseSignals(binaryPlaces)
+            return reverseIfNeeded(binaryPlaces, baseList)
         }
 
-        private fun mapBinaryPlacesToSignals(binaryPlaces: List<Int>): List<Signal> {
-            val baseList = (0..3).mapNotNull {
-                if (binaryPlaces[it] != 0) mapBinaryPlaceToSignal[it] else null
-            }
-            return if (binaryPlaces[4] != 0) baseList.asReversed() else baseList
-        }
+        private fun baseSignals(binaryPlaces: List<Int>) =
+                binaryPlaces.mapNotNull { if (it != 0) mapBinaryPlaceToSignal[it] else null }
+
+        private fun reverseIfNeeded(binaryPlaces: List<Int>, baseList: List<Signal>) =
+                if (binaryPlaces.last() != 0) baseList.asReversed() else baseList
 
         private fun intToBinaryPlaceValues(num: Int): List<Int> {
-            return powersOfTwo.mapTo(mutableListOf(), { powerOfTwo -> num and powerOfTwo })
+            return mapBinaryPlaceToSignal.keys.mapTo(mutableListOf(), { powerOfTwo -> num and powerOfTwo })
         }
 
-        private val powersOfTwo = listOf(1, 2, 4, 8, 16)
     }
 
 }
