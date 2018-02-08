d9586dbd8c93bde4e4fa4e20ff7cd10f8c393002
baob <coder@onesandthrees.com>
2018-02-08 20:08:43 +0000

    luhn - big rework, aiming for clarity in main function, detail abstracted away

--- kotlin/luhn/src/main/kotlin/Luhn.kt
+++ kotlin/luhn/src/main/kotlin/Luhn.kt
@@ -1,25 +1,31 @@
+import java.lang.Character.isWhitespace
+
 class Luhn {
     companion object {
-        private val nineToInt = '9'.toInt()
-        private val zeroToInt = '0'.toInt()
 
         fun isValid(numberUnSanitized: String): Boolean {
-            val number = numberUnSanitized.filter { it != ' ' }
+            val number = numberUnSanitized.filterNot(::isWhitespace)
 
-            if (number.length <= 1) return false
-            if (!number.all{ (zeroToInt..nineToInt).contains(it.toInt()) }) return false
+            if ( number.length <= 1 ) return false
+            if ( number.any{ it !in '0'..'9' } ) return false
 
-            val checksum = number.reversed().foldIndexed(0) { index: Int, accumulator: Int, digit: Char ->
-                val digitAsInt = digit.toString().toInt()
-                val digitDoubled = 2 * digitAsInt
-                val component: Int = when (index.isEven()) {
-                    true -> digitAsInt
-                    else -> if ( digitDoubled > 9 ) digitDoubled - 9 else digitDoubled }
-                accumulator + component
-            }
-            return checksum % 10 == 0
+            return number
+                    .reversed()
+                    .mapIndexed{ index, digit -> digit.luhnComponent(index) }
+                    .sum()
+                    .let{ it % 10 == 0 }
         }
     }
 }
 
 private fun Int.isEven(): Boolean = (this.rem(2) == 0)
+
+private fun Int.luhnComponentIfOdd() = if (2 * this > 9) 2 * this - 9 else 2 * this
+
+private fun Char.digitToInt() = toString().toInt()
+
+private fun Char.luhnComponent(index: Int) =
+        if (index.isEven()) digitToInt()
+        else                digitToInt().luhnComponentIfOdd()
+
+
