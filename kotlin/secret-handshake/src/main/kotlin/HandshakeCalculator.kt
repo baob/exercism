8ed8b81df7e65770e18f9d04adc46b70a2128f5c
baob <coder@onesandthrees.com>
2018-02-08 16:56:03 +0000

    secret-handshake - not on exercism - this assumes ordering in Signal.kt

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,13 +1,10 @@
+import kotlin.experimental.and
+
 class HandshakeCalculator {
 
     companion object {
 
-        private val mapBinaryPlaceToSignal = mapOf(
-                0 to Signal.WINK,
-                1 to Signal.DOUBLE_BLINK,
-                2 to Signal.CLOSE_YOUR_EYES,
-                3 to Signal.JUMP
-        )
+        val mapBinaryPlaceToSignal = Signal.values().map{ Pair(it.ordinal, it) }.toMap()
 
         fun calculateHandshake(i: Int): List<Signal> {
             val binaryPlaces = intToBinaryPlaceValues(i)
@@ -15,7 +12,9 @@ class HandshakeCalculator {
         }
 
         private fun mapBinaryPlacesToSignals(binaryPlaces: List<Int>): List<Signal> {
-            val baseList = (0..3).mapNotNull { if (binaryPlaces[it] != 0) mapBinaryPlaceToSignal[it] else null }
+            val baseList = (0..3).mapNotNull {
+                if (binaryPlaces[it] != 0) mapBinaryPlaceToSignal[it] else null
+            }
             return if (binaryPlaces[4] != 0) baseList.asReversed() else baseList
         }
 
