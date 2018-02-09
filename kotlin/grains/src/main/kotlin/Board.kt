980f90aaf00e548b6ed708033333e63d43cb2f9b
baob <coder@onesandthrees.com>
2018-02-09 14:56:50 +0000

    grains - tidy up

--- kotlin/grains/src/main/kotlin/Board.kt
+++ kotlin/grains/src/main/kotlin/Board.kt
@@ -1,15 +1,17 @@
 import java.math.BigInteger
-import java.math.BigInteger.*
+import java.math.BigInteger.ONE
+import java.math.BigInteger.ZERO
 
 object Board {
-    private val TWO = BigInteger("2")
 
     @JvmOverloads
     tailrec fun getGrainCountForSquare(i: Int, ret: BigInteger = ONE): BigInteger {
-        require (i in 1..64) { "Only integers between 1 and 64 (inclusive) are allowed" }
+        require (i in rangeOfSquares) { "Only integers between 1 and 64 (inclusive) are allowed" }
         if (i == 1) return ret
-        return getGrainCountForSquare( i - 1, TWO * ret )
+        return getGrainCountForSquare( i - 1, ret + ret )
     }
 
-    fun getTotalGrainCount() = (1..64).fold(ZERO) { acc, it -> acc + getGrainCountForSquare(it) }
+    fun getTotalGrainCount() = rangeOfSquares.fold(ZERO) { acc, it -> acc + getGrainCountForSquare(it) }
+
+    private val rangeOfSquares = 1..64
 }
