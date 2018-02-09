4362366f76c49d6723cc672c32c6e8ce70ed0e7f
baob <coder@onesandthrees.com>
2018-02-09 12:41:03 +0000

    largest-series-product - refactoring for clarity, not brevity

--- kotlin/largest-series-product/src/main/kotlin/Series.kt
+++ kotlin/largest-series-product/src/main/kotlin/Series.kt
@@ -5,18 +5,18 @@ class Series(private val number: String) {
     fun getLargestProduct(length: Int): Int {
         require (length >= 0) { "Length cannot be $length" }
 
-        val startRange = 0..number.length - length
+        val startRange = (0..number.length).takeWhile{ endOfRange(it, length) < number.length }
         require (!startRange.isEmpty()) { "product length $length too big for number length ${number.length}" }
 
         return startRange
-                .map { start -> product(start, length) }
+                .map { product(it, length) }
                 .sorted().last()
     }
 
     private fun product(start: Int, length: Int) =
-            subStringRange(start, length).map { numberToInt[it] }.fold(1) { a, b -> a * b }
-
-    private fun subStringRange(start: Int, length: Int) = (start..endOfRange(start, length))
+            (start..endOfRange(start, length))
+                    .map { numberToInt[it] }
+                    .fold(1) { a, b -> a * b }
 
     private fun endOfRange(start: Int, length: Int) = (start + length - 1)
 
